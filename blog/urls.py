c289b8ac80914487c4f0e74ad73bc60acf7e5417
Alexandre Guillaume Nicolas CAMBEFORT <acambefort@pentalog.com>
2020-09-24 16:59:45 +0300

    changed the post model

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('posts/', views.get_posts, name='posts')
+ path('posts/', views.get_posts, name='posts'),
+ path('posts/<str:url_slug>', views.get_post_by_slug, name='get_post_by_slug')
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,15 @@ from django.http import JsonResponse
 # Create your views here.
 
 
-def get_posts(requests):
+def get_posts(request):
     posts = Post.objects.all()
-    data = {'results': list(posts.values('title', 'content', 'published'))}
-    return JsonResponse(data)
+    context = {'posts':posts}
+    return render(request, 'blog/list.html', context)
+
+def get_post_by_slug(request, url_slug):
+    slug = url_slug
+    post = Post.objects.filter(slug=slug).first()
+    context = {'post':post}
+    return render(request, 'blog/post.html', context)
+
+
